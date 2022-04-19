698204152eb22fc09b794976e36bc489674c9365
kalapatidaniel <kalapatidaniel@gmail.com>
2022-04-19 14:50:38 +0200

    Final

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import datetime
 
-name: str = input("What is your name? ")
-print(f"{datetime.datetime.now()} - Hello {name}!")
+truefalse: bool = True
+
+
+def hello_xy(name: str = "asdfasgas"):
+    print("#"*50)
+
+    if truefalse:
+        print(True)
+
+    if name == "asdfasgas":
+        name = input("What is your name?")
+        if name == "asd":
+            print("asd")
+    else:
+        print("I know you!")
+    return f"{datetime.datetime.now()} - Hello {name}!"
+
+
+print(hello_xy())
+print(hello_xy("Pisti"))
